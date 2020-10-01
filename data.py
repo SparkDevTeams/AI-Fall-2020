794ee4c68388a74c03152e09db85caee152b2ba9
daniel ortiz <danielortiz6810@gmail.com>
2020-10-01 16:39:19 -0400

    Adding a chunked .csv file for formatting

--- data.py
+++ data.py
@@ -5,7 +5,8 @@ import json
 # df = pd.read_json(StringIO("Video_Games.json"), lines=True)
 # print(pd)
 
-with open('/Users/franM/projects/sparkdev_AI/AI-Fall-2020/Video_Games.json') as f:  #change to file directory for the json file
+with open('D:\danny\Coding\AI Fall 2020\AI GitHub\AI-Fall-2020\Video_Games.json') as f:  #change to file directory for the json file
     chunks = pd.read_json(f, lines=True, chunksize=100)
     for c in chunks:
-        print(c)
+        csvData = c.to_csv('Export.csv',index = False)
+        
