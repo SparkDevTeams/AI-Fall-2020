68815ebed196a04962b9c3735f2eefaf976e12fc
Franb57 <francisco57b@gmail.com>
2020-11-12 16:36:49 -0500

    added functionality to main file

--- Main.py
+++ Main.py
@@ -12,51 +12,82 @@ import stanza
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 all_stopwords = nlp.Defaults.stop_words
 
-
+col_list = ["reviewText"]
+df_og = pd.read_csv("Export1.csv", usecols=col_list)
 # Input and output CSV files
 IN_FILE_NAME = "Export1.csv"
 OUT_FILE_NAME = "Stop.csv"
 
+# print("Before Edits")
+# print(df_og.to_string(columns=col_list, index=False,header=False,max_rows=5))
 
-def stopwordRemover(sentence: str): 
+
+def stopwordRemover(column): 
     """Takes a string as an input and outputs the string without stopwords"""
-    # Parse the sentence using the loaded 'en' model object `nlp`
-    doc = nlp(sentence)
+    # Parse the sentence using the loaded 'en' model object `nlp`  
+   # """Takes a string as an input and outputs the string without stopwords"""
 
     # Join words that aren't stopwords
-    end = " ".join([token.text for token in doc if not token.lower_ in all_stopwords])
-    return end
-  
-
-def stopwords(IN_FILE_NAME:str, OUT_FILE_NAME:str):
-    """Takes input and output files to modify the reviewText column"""
-    with open(IN_FILE_NAME, newline='') as csvfile, open(OUT_FILE_NAME, 'w', newline='') as out_csv:
-        reader = csv.DictReader(csvfile)
-        csv_writer = csv.DictWriter(out_csv, fieldnames=reader.fieldnames)
-        csv_writer.writeheader()
-        for row in reader:
-            row['reviewText'] = stopwordRemover(row['reviewText'])
-            csv_writer.writerow(row)
-
-stopwords(IN_FILE_NAME, OUT_FILE_NAME)
-
-# Creating dataframe
-col_list = ["reviewText"]
-df = pd.read_csv("Stop.csv", usecols=col_list)
+    new_words = []
+    for row in column:
+        new_words.append(" ".join([word for word in (str(row).split()) if word.lower() not in all_stopwords]))
+    return new_words
+   
+    
+    
 
 ###################
 # Removing Punctuation
 exclude = set(string.punctuation)
 
-def testRemove(sentence):
+def removePunctuation(sentence):
    for ele in sentence:
       if (ele in exclude):
-         sentence = sentence.replace(ele, "")
+         sentence = sentence.replace(ele, " ")
    return sentence
 
-#Punctuation remove
-testRemove(df.to_string(columns=col_list, index=False,header=False,max_rows=5))  
+# reviews_cleaned = df_og.copy()
 
-#Remove Digits
-df['reviewText'] = df['reviewText'].str.replace(r'\d+', '')
+test = "The game itself worked!! great but the story line videos would never play, the sound was fine but the picture!! would freeze and go black every time."
+test = removePunctuation(test)
+print(test)
+
+
+
+
+#print("Before: ", df_og['reviewText'].head(10))
+
+df_og['reviewText'] = df_og['reviewText'].str.replace(r'\d+', '')  #removing digits 
+
+df_og['reviewText'] = stopwordRemover(df_og["reviewText"].values)
+df_og['reviewText'] = df_og['reviewText'].str.replace('[^\w\s]','')
+#df_og['reviewText'] = removePunctuation(df_og["reviewText"].values)
+
+print(df_og.head(10))
+
+df_og.to_csv("Cleanedre.csv")
+
+#df_og['reviewText'] = stopwordRemover(df_og["reviewText"].head(10))
+#print(df_og['reviewText'].to_numpy(dtype=str))
+
+#print("Stopwords")
+
+#print(stopwordRemover(df_og.to_string(columns=col_list, index=False,header=False,max_rows=10)))
+
+#print("After:", df_og['reviewText'].head(10))
+
+# print(testRemove(test))
+# print(stopwordRemover(test))
+# #Punctuation remove
+# print("Remove Punctuation")
+# testRemove(df_og.to_string(columns=col_list, index=False,header=False,max_rows=5))
+
+
+
+
+# #Remove Digits
+
+# stopwordRemover(df_og.to_string(columns=col_list, index=False,header=False,max_rows=5))
+
+# print(df_og.to_string(columns=col_list, index=False,header=False,max_rows=5))
 
