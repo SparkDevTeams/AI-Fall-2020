f28d64303ca00935847de8e64de2d6e27ea8cd66
rlope265 <rlope265@fiu.edu>
2020-11-14 13:58:23 -0500

    Update vectorization.py
    Updated vectorization.py to test for overall score

--- vectorization.py
+++ vectorization.py
@@ -14,36 +14,37 @@ import numpy as np
 system("mkdir 'data_preprocessors'")
 system("mkdir 'vectorized_data'")
 
-col_list = ["reviewText"]
-df_og = pd.read_csv("Cleanedre.csv", usecols=col_list)
+col_list = ["overall", "reviewText"]
+df_og = pd.read_csv("Cleaned_million1.csv", usecols=col_list)
 
-#def vectorization(pdCol: np.array):
-"""Bigram vectorizer that has a pdDataFram["columnName"].values as input"""
+def vectorization():
+    """Bigram vectorizer that has a pdDataFram["columnName"].values as input"""
 
-# Bigram Counts
-bigram_vectorizer = CountVectorizer(ngram_range=(1, 2))
-bigram_vectorizer.fit(df_og["reviewText"].values.astype('U'))
+    # Bigram Counts
+    bigram_vectorizer = CountVectorizer(ngram_range=(1, 2))
+    bigram_vectorizer.fit(df_og["reviewText"].values.astype('U'))
 
-dump(bigram_vectorizer, 'data_preprocessors/bigram_vectorizer.joblib')
-# bigram_vectorizer = load('data_preprocessors/bigram_vectorizer.joblib')
+    dump(bigram_vectorizer, 'data_preprocessors/bigram_vectorizer.joblib')
+    # bigram_vectorizer = load('data_preprocessors/bigram_vectorizer.joblib')
 
-X_train_bigram = bigram_vectorizer.transform(df_og["reviewText"].values.astype('U'))
+    X_train_bigram = bigram_vectorizer.transform(df_og["reviewText"].values.astype('U'))
 
-save_npz('vectorized_data/X_train_bigram.npz', X_train_bigram)
-# X_train_bigram = load_npz('vectorized_data/X_train_bigram.npz')
+    save_npz('vectorized_data/X_train_bigram.npz', X_train_bigram)
+    # X_train_bigram = load_npz('vectorized_data/X_train_bigram.npz')
 
 
-# Bigram Tf-Idf
-bigram_tf_idf_transformer = TfidfTransformer()
-bigram_tf_idf_transformer.fit(X_train_bigram)
+    # Bigram Tf-Idf
+    bigram_tf_idf_transformer = TfidfTransformer()
+    bigram_tf_idf_transformer.fit(X_train_bigram)
 
-dump(bigram_tf_idf_transformer, 'data_preprocessors/bigram_tf_idf_transformer.joblib')
-# bigram_tf_idf_transformer = load('data_preprocessors/bigram_tf_idf_transformer.joblib')
+    dump(bigram_tf_idf_transformer, 'data_preprocessors/bigram_tf_idf_transformer.joblib')
+    # bigram_tf_idf_transformer = load('data_preprocessors/bigram_tf_idf_transformer.joblib')
 
-X_train_bigram_tf_idf = bigram_tf_idf_transformer.transform(X_train_bigram)
-save_npz('vectorized_data/X_train_bigram_tf_idf.npz', X_train_bigram_tf_idf)
-# X_train_bigram_tf_idf = load_npz('vectorized_data/X_train_bigram_tf_idf.npz')
+    X_train_bigram_tf_idf = bigram_tf_idf_transformer.transform(X_train_bigram)
+    save_npz('vectorized_data/X_train_bigram_tf_idf.npz', X_train_bigram_tf_idf)
+    # X_train_bigram_tf_idf = load_npz('vectorized_data/X_train_bigram_tf_idf.npz')
 
+#vectorization()
 
 def train_and_show_scores(X: csr_matrix, y: np.array, title: str) -> None:
         X_train, X_valid, y_train, y_valid = train_test_split(
@@ -56,4 +57,10 @@ def train_and_show_scores(X: csr_matrix, y: np.array, title: str) -> None:
         valid_score = clf.score(X_valid, y_valid)
         print(f'{title}\nTrain score: {round(train_score, 2)} ; Validation score: {round(valid_score, 2)}\n')
 
-#print(vectorization(df_og["reviewText"].values.astype('U')))
+
+X_train_bigram_tf_idf = load_npz('vectorized_data/X_train_bigram_tf_idf.npz')
+y_train = df_og["overall"].values
+y_train[y_train <= 2] = 0
+y_train[y_train >= 3] = 1
+
+train_and_show_scores(X_train_bigram_tf_idf, y_train, "Bigram TFIDF")
